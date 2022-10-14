d3ff3e8283e0e247ebe2a5f4fb2daf91e01d1b73
pierreportal2 <32952086+pierreportal2@users.noreply.github.com>
2022-10-14 09:37:54 +0200

    Update clipboard.sh

--- clipboard.sh
+++ clipboard.sh
@@ -1,5 +1,5 @@
-TESTTESTTEST
-TESTTESTTEST
-TESTTESTTEST
-TESTTESTTEST
-TESTTESTTESTTESTTESTTEST
+# clone this repo
+git clone https://github.com/sassoftware/viya4-iac-k8s
+
+# move to the project directory
+cd viya4-iac-k8s
