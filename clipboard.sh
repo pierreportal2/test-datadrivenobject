68dd15a700124e6b9262cebc405bd2c615f02bc9
pierreportal2 <32952086+pierreportal2@users.noreply.github.com>
2022-10-14 09:40:59 +0200

    Update clipboard.sh

--- clipboard.sh
+++ clipboard.sh
@@ -1,5 +1,3 @@
-# clone this repo
-git clone https://github.com/sassoftware/viya4-iac-k8s
-
-# move to the project directory
-cd viya4-iac-k8s
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
